688f6b8e50c1e22e137df6371b66ad0060c8833a
Krittika2003 <92219310+Krittika2003@users.noreply.github.com>
2024-02-01 00:54:41 +0530

    Add files via upload

---
+++ loginMember.java
@@ -0,0 +1,33 @@
+package projectLogin;
+
+public class loginMember {
+	private String uname, password;
+
+	public loginMember() {
+		super();
+	}
+
+	public loginMember(String uname, String password) {
+		super();
+		this.uname = uname;
+		this.password = password;
+	}
+
+	public String getUname() {
+		return uname;
+	}
+
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
+
---
+++ loginRegisterDao.java
@@ -0,0 +1,56 @@
+package projectLogin;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class loginRegisterDao {
+
+	private String dbUrl = "jdbc:mysql://localhost:3306/tracker";
+	private String dbUname = "root";
+	private String dbPassword = "Krittik@123";
+	private String dbDriver = "com.mysql.cj.jdbc.Driver";
+
+	public void loadDriver(String dbDriver) {
+		try {
+			Class.forName(dbDriver);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public Connection getConnection() {
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(dbUrl, dbUname, dbPassword);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	public String insert(loginMember myuser) {
+		loadDriver(dbDriver);
+		Connection con = getConnection();
+		String result = "Data entered successfully";
+		String sql = "insert into myuser values(?,?)";
+
+		PreparedStatement ps;
+		try {
+			ps = con.prepareStatement(sql);
+			ps.setString(1, myuser.getUname());
+			ps.setString(2, myuser.getPassword());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			result = "Data not entered";
+		}
+		return result;
+	}
+
+}
+
